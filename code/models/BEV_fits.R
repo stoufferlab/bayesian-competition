3d42d25b63c6549b37da16111e1a0ee940aa92b4
albaricoque <albaricoque.cl.93@gmail.com>
2020-06-02 13:23:12 +1200

    Some code on models and on trying to figure out what the feasibility domain is for 2 sp. Somehow it gives different values than when we use the Saavedra code. Hmpf

--- code/models/BEV_fits.R
+++ code/models/BEV_fits.R
@@ -8,15 +8,15 @@ trcy_focal<-vero_trcy[which(vero_trcy$focal=="T"),]
 
 # The multilevel model that I do not want to use because I am not sure it is the right approach
 
- BEV_vero_multi<-brm(bf(totalseeds~ lambdai /(1 + (alphaii*verodensity) + (alphaij*trcydensity) ),
-                       lambdai ~ 1 + (1|place|label),
-                       alphaii ~ 1  + (1|place|label),
-                       alphaij ~1  + (1|place|label),
-                      nl=T),
-                    data=vero_focal,family=poisson(),
-                    iter = 5000, warmup = 3000, cores = 4, inits=0,control =list(adapt_delta = .99)
- )
- saveRDS(BEV_vero,file="BEV_vero_multi.RDS")
+ # BEV_vero_multi<-brm(bf(totalseeds~ lambdai /(1 + (alphaii*verodensity) + (alphaij*trcydensity) ),
+ #                       lambdai ~ 1 + (1|place|label),
+ #                       alphaii ~ 1  + (1|place|label),
+ #                       alphaij ~1  + (1|place|label),
+ #                      nl=T),
+ #                    data=vero_focal,family=poisson(),
+ #                    iter = 5000, warmup = 3000, cores = 4, inits=0,control =list(adapt_delta = .99)
+ # )
+ # saveRDS(BEV_vero,file="BEV_vero_multi.RDS")
 
 
 
@@ -35,7 +35,7 @@ trcy_focal<-vero_trcy[which(vero_trcy$focal=="T"),]
 # 
 # 
 
-pp_2 = c(
+prior_vero = c(
   prior(normal(0,10), nlpar = "alphaii"),
   prior(normal(0, 10), nlpar = "alphaij"),
   prior(normal(0, 100), nlpar = "lambdai")
@@ -47,18 +47,17 @@ BEV_vero<-brm(bf(totalseeds~ lambdai /(1 + (alphaii*verodensity) + (alphaij*trcy
                   alphaii ~ 1  ,
                   alphaij ~1 ,
                   nl=T),
-               prior = pp_2,
+               prior = prior_vero,
                data=vero_focal,family=poisson(),
                iter = 4000, warmup = 2000, cores = 4, inits=0,control =list(adapt_delta = .99)
 )
 
 saveRDS(BEV_vero,file="BEV_vero.RDS")
 
-
-pp_3 = c(
-  prior(normal(0,10), nlpar = "alphaji"),
-  prior(normal(0, 10), nlpar = "alphajj"),
-  prior(normal(0, 100), nlpar = "lambdaj")
+prior_trcy = c(
+  prior(normal(0,10), nlpar = "alphajj"),
+  prior(normal(0, 10), nlpar = "alphaji"),
+  prior(normal(0, 10), nlpar = "lambdaj")
 )
 
 BEV_trcy<-brm(bf(totalseeds~ lambdaj /(1 + (alphaji*verodensity) + (alphajj*trcydensity) ),
@@ -66,7 +65,7 @@ BEV_trcy<-brm(bf(totalseeds~ lambdaj /(1 + (alphaji*verodensity) + (alphajj*trcy
                  alphaji ~ 1  ,
                  alphajj ~1 ,
                  nl=T),
-              prior = pp_3,
+              prior = prior_trcy,
               data=trcy_focal,family=poisson(),
               iter = 4000, warmup = 2000, cores = 4, inits=0,control =list(adapt_delta = .99)
 )
@@ -76,3 +75,23 @@ saveRDS(BEV_trcy,file="BEV_trcy.RDS")
 
 
 
+
+
+
+
+BEV_trcy_multi<-brm(bf(totalseeds~ lambdaj /(1 + (alphaji*verodensity) + (alphajj*trcydensity) ),
+                 lambdaj ~ 1 + (1|place|label) ,
+                 alphaji ~ 1 + (1|place|label) ,
+                 alphajj ~1  + (1|place|label),
+                 nl=T),
+              data=trcy_focal,family=poisson(),
+              iter = 4000, warmup = 2000, cores = 4, inits=0,control =list(adapt_delta = .99)
+)
+
+saveRDS(BEV_trcy_multi,file="BEV_trcy_multi.RDS")
+
+
+
+library(ggplot2)
+ggplot(vero_focal) + geom_line(mapping=aes(x=verodensity,y=totalseeds,col=exposedenv))
+ggplot(trcy_focal) + geom_line(mapping=aes(x=trcydensity,y=totalseeds,col=exposedenv))

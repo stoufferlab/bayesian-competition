746f8f46050dd865c07cc5ed35e6c254b9f894f0
Alba Cervantes <albicoque.cl.93@gmail.com>
2021-01-18 16:01:05 +1300

    code to determine shortest distance to the bounds of the feasibility domain

--- code/determine_boundary.R
+++ code/determine_boundary.R
@@ -1,6 +1,9 @@
 require("sp")
+require("SpatialGraph")
+require("tidyverse")
 source("code/integration_toolbox.R")
 
+#This function determines the boundary of feasibility for a vector of angle theta
 # R_max is the maximum radius calculated for a particular combo of constraints and alpha matrix
 feasibility_boundary <- function(theta,
                                  alpha, 
@@ -40,101 +43,21 @@ feasibility_boundary <- function(theta,
                        Nupper = Nupper)
   
                      R_bounds <- do.call(rbind, R_boundary)
-                     
-                    # #which values are unfeasible? 
-                    #  unfeasible <- filter(R_bounds, feasible == 0)
-                    #   
-                    #  #if none, then return the maximum value of R
-                    #  if(nrow(unfeasible) == 0){
-                    #    Rb <- R_bounds[which(R_bounds$R_bound == max(R_bounds$R_bound) ), ]
-                    #    bounds <- data.frame("theta"= theta, 
-                    #                         "R_bound"= Rb[,"R_bound"],
-                    #                         "ri"= Rb[,"ri"],
-                    #                         "rj"= Rb[,"rj"])
-                    #    return(bounds)
-                    #  }else{
-                    #    #if not return the minimum value of R where the growth rates are no longer feasible 
-                    #    Rb <- unfeasible[which(unfeasible$R_bound == min(unfeasible$R_bound) ), ]
-                    #    bounds <- data.frame("theta"= theta, 
-                    #                         "R_bound"= Rb[,"R_bound"],
-                    #                         "ri"= Rb[,"ri"],
-                    #                         "rj"= Rb[,"rj"])
-                    #    return(bounds)
-                    #  }
+                     feasible_bounds <- filter(R_bounds, feasible ==1) 
+                     return(feasible_bounds)
                      
 }
 
-alpha_boundary <- function(alpha,
-                           R_max,
-                           rconstraints=NULL,
-                           Nupper=NULL){
-  # we add the shape delimited by the alphas alone
-  # The column vectors of the alpha matrix define the boundary of the feasibility domain 
-  # minus a tiny fraction because exactly in the boundary the feasibility is 0!
-  m1 <- (alpha[2,2] / alpha[1,2]) - 1e-5 
-  m2 <- alpha[2,1] / alpha[1,1]   - 1e-3
-  
-  values <- seq(0, R_max, length.out = 1000) %>% as.list()
-  
-  upper_limit <- lapply(values, function(ri,
-                                             alpha,
-                                             R_max, 
-                                             rconstraints= rconstraints,
-                                             Nupper= Nupper){
-    rj <- ri*m1
-    r<- c(ri, rj)
-     feas <- check_feasibility(r = c(ri, rj),
-                               alpha = alpha, 
-                               rconstraints = rconstraints,
-                               Nupper =  Nupper )
-     if(feas){
-       upper_limit<- data.frame("ri"=ri, "rj"=rj)
-       return(upper_limit)
-     }else{
-       NULL
-     }
-    
-    
-  }, alpha=alpha,
-  R_max =  R_max,
-  rconstraints =  rconstraints,
-  Nupper= Nupper)
-  #now for the lower  defined by m2
-  lower_limit <- lapply(values, function(ri,
-                                         alpha,
-                                         R_max, 
-                                         rconstraints= rconstraints,
-                                         Nupper= Nupper){
-    rj <- ri*m2
-    r<- c(ri, rj)
-    feas <- check_feasibility(r = r,
-                              alpha = alpha, 
-                              rconstraints = rconstraints,
-                              Nupper =  Nupper )
-    if(feas){
-      upper_limit<- data.frame("ri"=ri, "rj"=rj)
-      return(upper_limit)
-    }else{
-      NULL
-    }
-    
-    
-  }, alpha=alpha,
-  R_max =  R_max,
-  rconstraints =  rconstraints,
-  Nupper= Nupper)
-  
-  upper_points <- do.call(rbind, upper_limit)
-  lower_points <- do.call(rbind, lower_limit)
-}
 
+# this function iteraties over a series of thetas to determine the shape and bounds of the feasibility domain
 feasibility_shape<-function( alpha, 
                              R_max,
                              rconstraints=NULL,
                              Nupper=NULL){
-  N <- 500
+  N <- 100
   thetas <- seq(0 , 2*pi, length.out = N) %>% as.list()
   
+  #we apply the feasibility_boundary function to a set of thetas
   bound <- lapply(thetas, function(t,
                                     alpha,
                                     R_max, 
@@ -155,52 +78,77 @@ feasibility_shape<-function( alpha,
   Nupper= Nupper)
   
   shape <- do.call(rbind, bound)
+   
+  #we determine the boundary of the points
+  coord_points <- chull(x = shape[,"ri"], y=shape[,"rj"]) 
+  #we add the first point to complete the polygon
+  coord_points<- c(coord_points, coord_points[1])
+  shape_bounds <- shape[coord_points,]
   
-  # we add the shape delimited by the alphas alone
-  # The column vectors of the alpha matrix define the boundary of the feasibility domain 
-  s1 <- alpha[2,2] / alpha[1,2]
-  s2 <- alpha[2,1] / alpha[1,1]
-  
-  limit_rj <- shape[1,]
-  ri_s1 <- seq(0, limit_rj[,"ri"], length.out = N/2)
-  rj_s1 <- ri_s1 * s1
-  
-  upper_limit <- data.frame("ri"= ri_s1, "rj"=rj_s1)
-  
-  limit_ri <- filter(shape, ri == min(rj))
-  ri_s2 <- seq(0, limit_rj[,"ri"], length.out = N/2)
-  rj_s2 <- ri_s1 * s2
-  
-  lower_limit <- data.frame("ri"= ri_s2, "rj"=rj_s2)
-  # All together, these coordinates describe the conical hull 
-  
-  coordinates <- shape[,c("ri","rj")] %>% rbind( upper_limit,lower_limit)
-  
-  return(coordinates)
+  return(shape_bounds)
 }
 
 
-alpha <- diag(2)
-alpha[1,2] <- 0.3
-alpha[2,1] <- 0.5
-R_max <- 1000
-rconstraints <- list(
-  lower = c(-Inf, -Inf),
-  upper = c(Inf, Inf)
-)
+#function that returns the shortest distance from the point defined by the growth rates and 
+#the bounds of the feasibility domain
+#r is a vector of growth rates
+#shape is the bounds of the feasibility domain, defined by the funciton feasibility_shape
 
-#And each species its maximum expected abundances
-Nupper <- c(i = 200,
-            j = 300)
+shortest_distance<-function(r, shape){
+  N <- nrow(shape) -1
+  distances <- c()
+  cc <- c()
+  for( i in 1:N){
+    
+    p0 <- c(shape[i,]$ri, shape[i,]$rj)
+    p1 <- c(shape[i+1,]$ri, shape[i+1,]$rj)
+    
+    line_matrix <- rbind(p0,p1) %>% as.matrix()
+    r <- as.matrix(r) %>% t
+    dist<- SpatialGraph::pointLineD(xy = line_matrix,
+                                    xyp = r )
+    dist <- dist$d
+    
+    data <- data.frame("distance"= dist,
+                       #"distance_point1"= dist_pt1,
+                       #"distance_point2"= dist_pt2,
+                       "p1x"=p0[1],
+                       "p1y"=p0[2],
+                       "p2x"=p1[1],
+                       "p2y"=p1[2])
+    
+    
+    distances <- rbind(distances, data)
+    
+  }
+  
+  minimum_distance <- distances[which(distances$dist==min(distances$dist)),]
+  # points(r[1], r[2], col="blue", pch=20)
+  # lines(x = c(minimum_distance[,"p1x"], minimum_distance[,"p2x"]),
+  #       y=c(minimum_distance[,"p1y"], minimum_distance[,"p2y"]), 
+  #       lwd=2, 
+  #       col="darkgoldenrod")
+ return(min(distances$dist))
+  #return(minimum_distance)
+}
 
-r_feasible(alpha = alpha, rconstraints = rconstraints, Nupper = Nupper, R_max = R_max, make_plot = TRUE)
 
 
-
-
-gg <- feasibility_shape(alpha = alpha , R_max = R_max , rconstraints = rconstraints, Nupper = Nupper)
-
-points(gg$ri, gg$rj, pch=20, col= "dodgerblue")
-
+#wrapper of the functions defined above for it's usefull implementation
+#its output is the shortest distance from the point of growth rates to the bounds of the feasibility domain
+distance_from_limit <- function(alpha, 
+                                R_max,
+                                rconstraints=NULL,
+                                Nupper=NULL,
+                                r){
+  shape <- feasibility_shape(alpha = alpha,
+                             R_max = R_max,
+                             rconstraints = rconstraints, 
+                             Nupper = Nupper)
+  
+  distance <- shortest_distance(r = r,
+                                shape = shape)
+  return(distance)
+}
 
 

03f6ea9283b80bbbf594dd00bf0d021ccdc0216f
Alba Cervantes <albicoque.cl.93@gmail.com>
2021-08-13 17:38:20 +1200

    parameter sensitivity

--- code/parameter_sensitivity_figures.R
+++ code/parameter_sensitivity_figures.R
@@ -1,10 +1,12 @@
 library(ggplot2)
 library(ggpubr)
+library(tidyverse)
 
-bh_bh_sensitiviyt <- read_csv("bh_bh_sensitiviyt.csv")
 
-xx <- expression("Relative coexistence ratio,"~rho)
-yy <- expression("Distance from the edge,"~delta)
+bh_bh_sensitiviyt <- read.csv("bh_bh_sensitiviyt.csv")
+
+xx <- expression("A) Relative coexistence ratio,"~rho)
+yy <- expression("B) Distance from the edge,"~delta)
 
 parameters <- c( expression(alpha[ii]),
                  expression(alpha[ij]),
@@ -17,25 +19,102 @@ parameters <- c( expression(alpha[ii]),
 
 
 
-sunny <- read_csv("sunny.csv")
+sunny <- read.csv("sunny.csv")
 
 sunny <- sunny %>% filter(
   vero_model == "Beverton-Holt",
   trcy_model == "Beverton-Holt"
 )
 
-mean(sunny$area_alone)
+woody <- read.csv("woody.csv")
+
+woody <- woody %>% filter(
+  vero_model == "Beverton-Holt",
+  trcy_model == "Beverton-Holt"
+)
+
+results <- read.csv("results.csv")
+
+results <- results %>% filter(
+  vero_model == "Beverton-Holt",
+  trcy_model == "Beverton-Holt"
+)
+
+
+
+area_feasible_mean <-
+  data.frame(
+    "area_feasible" = c(
+      unique(sunny$area_mean),
+      unique(woody$area_mean)
+    ),
+    "environment" = c("open", "woody")
+  )
+
+N <- nrow(bh_bh_sensitiviyt)/2
+
+area_feasible_mean <- do.call("rbind", replicate(N, area_feasible_mean, simplify = FALSE))
+
+
+
+area_alone_mean <-
+  data.frame(
+    "area_alone" = c(
+      unique(sunny$area_alone_mean),
+      unique(woody$area_alone_mean)
+    ),
+    "environment" = c("open", "woody")
+  )
+
+
+area_alone_mean <- do.call("rbind", replicate(N, area_alone_mean, simplify = FALSE))
+
+
+
+
+
+proportion_mean <-
+  data.frame(
+    "proportion_mean_value" = c(
+      unique(sunny$proportion_mean),
+      unique(woody$proportion_mean)
+    ),
+    "environment" = c("open", "woody")
+  )
+
+
+proportion_mean <- do.call("rbind", replicate(N, proportion_mean, simplify = FALSE))
+
+
+
+
+
+
+distance_mean <-
+  data.frame(
+    "distance_mean_value" = c(
+      unique(sunny$distance_mean),
+      unique(woody$distance_mean)
+    ),
+    "environment" = c("open", "woody")
+  )
+
+distance_mean <- do.call("rbind", replicate(N, distance_mean, simplify = FALSE))
+
 
 
 # area_feasible -----------------------------------------------------------
 
-
-
-area_together <-ggplot(bh_bh_sensitiviyt)+
+area_together <-  ggplot(bh_bh_sensitiviyt)+
   geom_boxplot(aes(x=parameter,
                    y=area_feasible),
-               fill)+
+               outlier.alpha = 0.5,
+               outlier.size = 0.5)+
   theme_bw()+
+  geom_abline(data = area_feasible_mean,
+              aes(intercept = area_feasible, slope=0)
+             )+
+  facet_grid(~environment)+
   theme(
     strip.background = element_rect(fill = "white"),
     panel.grid.major = element_blank(),
@@ -49,13 +128,9 @@ area_together <-ggplot(bh_bh_sensitiviyt)+
     legend.box = "vertical",
     legend.margin = margin()
   ) +
-  geom_abline(intercept = unique(sunny$area_mean),
-              slope = 0,
-              col = "grey50",
-              linetype = "dashed",
-              size = 1.3)+
   scale_x_discrete(labels=parameters)+
-  ggtitle(  expression("Biologically-constrained feasibility domain,"~beta))
+  ggtitle(  expression("A) Biologically-constrained feasibility domain,"~beta))
+ 
 
 
 
@@ -63,10 +138,15 @@ area_together <-ggplot(bh_bh_sensitiviyt)+
 # area alone ----------------------------------------------------------------
 
 
-area_alone <-ggplot(bh_bh_sensitiviyt)+
+area_alone <- ggplot(bh_bh_sensitiviyt)+
   geom_boxplot(aes(x=parameter,
                    y=area_alone),
-               fill)+
+               outlier.alpha = 0.5,
+               outlier.size = 0.5)+
+  geom_abline(data = area_alone_mean,
+              aes(intercept = area_alone, slope=0)
+  )+
+  facet_grid(~environment)+
   theme_bw()+
   theme(
     strip.background = element_rect(fill = "white"),
@@ -81,13 +161,10 @@ area_alone <-ggplot(bh_bh_sensitiviyt)+
     legend.box = "vertical",
     legend.margin = margin()
   ) +
-  geom_abline(intercept = unique(sunny$area_alone_mean),
-              slope = 0,
-              col = "grey50",
-              linetype = "dashed",
-              size = 1.3)+
   scale_x_discrete(labels=parameters)+
-  ggtitle( expression("Area in monocolture,"~gamma))
+  ggtitle( expression("B) Area in monocolture,"~gamma))
+
+
 
 
 # proportion --------------------------------------------------------------
@@ -96,7 +173,12 @@ area_alone <-ggplot(bh_bh_sensitiviyt)+
 rho <- ggplot(bh_bh_sensitiviyt)+
   geom_boxplot(aes(x=parameter,
                    y=proportion),
-               fill)+
+               outlier.alpha = 0.5,
+               outlier.size = 0.5)+
+  geom_abline(data = proportion_mean,
+              aes(intercept = proportion_mean_value, slope=0)
+  )+
+  facet_grid(~environment)+
   theme_bw()+
   theme(
     strip.background = element_rect(fill = "white"),
@@ -111,21 +193,25 @@ rho <- ggplot(bh_bh_sensitiviyt)+
     legend.box = "vertical",
     legend.margin = margin()
   ) +
-  geom_abline(intercept = unique(sunny$proportion_mean),
-              slope = 0,
-              col = "grey50",
-              linetype = "dashed",
-              size = 1.3)+
   scale_x_discrete(labels=parameters)+
-  ggtitle(xx)
+  ggtitle(xx)+
+  ylim(0,2.5)
 
 # distance ----------------------------------------------------------------
 
 
-delta <-ggplot(bh_bh_sensitiviyt)+
+delta <-
+  
+  ggplot(bh_bh_sensitiviyt)+
   geom_boxplot(aes(x=parameter,
-                   y=distance)
+                   y=distance),
+               outlier.alpha = 0.5,
+               outlier.size = 0.5
                )+
+  geom_abline(data = distance_mean,
+              aes(intercept = distance_mean_value, slope=0)
+  )+
+  facet_grid(~environment)+
   theme_bw()+
   theme(
     strip.background = element_rect(fill = "white"),
@@ -140,19 +226,45 @@ delta <-ggplot(bh_bh_sensitiviyt)+
     legend.box = "vertical",
     legend.margin = margin()
   ) +
-  geom_abline(intercept = unique(sunny$distance_mean),
-              slope = 0,
-              col = "grey50",
-              linetype = "dashed",
-              size = 1.3)+
   scale_x_discrete(labels=parameters)+
   ggtitle(yy)
 
 
 # -------------------------------------------------------------------------
 
-ggarrange(area_together, area_alone,
+rho_delta <- ggarrange(
           rho, delta,
-          ncol = 2,
+          ncol = 1,
           nrow = 2)
+
+
+
+rho_delta<- annotate_figure(rho_delta,
+                         bottom = text_grob( "Parameter", size = 15),
+                         left  = text_grob( "Value", size = 15,rot=90)
+)
+
+
+
+
+
+areas <- ggarrange(
+ area_together, area_alone,
+  ncol = 1,
+  nrow = 2)
+
+
+
+areas<- annotate_figure(areas,
+                            bottom = text_grob( "Parameter", size = 15),
+                            left  = text_grob( "Value", size = 15,rot=90)
+)
+
+
+
+ggsave(rho_delta, filename = "../bayesian_competition_ms/params_results.pdf", width = 7, height = 6)
+
+ggsave(areas, filename = "../bayesian_competition_ms/areas_results.pdf", width = 7, height = 6)
+
+
 

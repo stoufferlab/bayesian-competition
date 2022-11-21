d43c67bd803d63d4aa206cc624268d513dd8fa6f
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2022-11-21 16:00:49 +1300

    removing priors for models that are not considered

--- code/models/set_priors.R
+++ code/models/set_priors.R
@@ -1,29 +1,28 @@
-#Prepare data set of VERO vs TRCY
-#set data and priors for fitting brms models with the environment as the fixed effect. 
+
+#set prior distributions for brms models
 library(brms)
 
-# #Set the priors all models are going to use 
-# prior_pairs = c(
-#   prior(normal(0, 10), nlpar = "alphaii"),
-#   prior(normal(0, 10), nlpar = "alphaij"),
-#   prior(normal(0, 10), nlpar = "lambda")
-# )
-
 prior_multi= c(
-  prior(normal(0, 0.1), nlpar = "alphaii"),
-  prior(normal(0, 0.1), nlpar = "alphaij"),
   prior(normal(0, 1), nlpar = "lambda"),
+  prior(normal(0, 0.1), nlpar = "alphaii"),
+  prior(normal(0, 0.1), nlpar = "alphaij"),  
   prior(normal(0, 0.1), nlpar = "alphaik")
 )
 
+# prior_exp_multi = c(
+#   prior(normal(0, 10), nlpar = "lambda"),
+#   prior(normal(0, 10), nlpar = "alphaii"),
+#   prior(normal(0, 10), nlpar = "alphaij"),
+#   prior(normal(0, 10), nlpar = "alphaik")
+#   # prior(normal(0,  0.1), nlpar = "beta")
+# )
 
-prior_lotka= c(
-  prior(normal(0, 0.01), nlpar = "alphaii"),
-  prior(normal(0, 0.01), nlpar = "alphaij"),
-  prior(normal(0, 0.1), nlpar = "lambda"),
-  prior(normal(0, 0.01), nlpar = "alphaik")
-)
-
+# prior_lotka= c(
+#   prior(normal(0, 0.01), nlpar = "alphaii"),
+#   prior(normal(0, 0.01), nlpar = "alphaij"),
+#   prior(normal(0, 0.1), nlpar = "lambda"),
+#   prior(normal(0, 0.01), nlpar = "alphaik")
+# )
 
 # 
 # prior_exp_pairs = c(
@@ -33,13 +32,3 @@ prior_lotka= c(
 #   prior(normal(0, .1 ), nlpar = "beta")
 # )
 # 
-
-
- prior_exp_multi = c(
-   prior(normal(0, 10), nlpar = "alphaii"),
-   prior(normal(0, 10), nlpar = "alphaij"),
-   prior(normal(0, 10), nlpar = "lambda"),
-  prior(normal(0,  0.1), nlpar = "beta"),
-   prior(normal(0, 10), nlpar = "alphaik")
- )
-

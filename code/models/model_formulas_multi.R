270f1dfb596de6a8b5507d106542bd06b85401fe
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2022-11-21 16:01:09 +1300

    removing superfluous models and adding comments

--- code/models/model_formulas_multi.R
+++ code/models/model_formulas_multi.R
@@ -1,5 +1,7 @@
 
+# nonlinear model structures for various phenomenological models of density-dependent fecundity
 
+# BEVERTON-HOLT MODEL
 beverton_holt_multi<-bf(
   totalseeds~ ( exp(lambda) /(1 + (alphaii*conspecifics) + (alphaij*heterospecifics) + (alphaik*totalother)) ) ,
   lambda  ~ 1 + env,
@@ -8,7 +10,7 @@ beverton_holt_multi<-bf(
   alphaik ~ 1 + env,
   nl=T)
 
-
+# RICKER MODEL
 ricker_multi<- bf(
   totalseeds~ exp(lambda) *exp( -(alphaii*conspecifics)  - (alphaij*heterospecifics) - (alphaik*totalother))  ,
   lambda  ~ 1 + env,
@@ -17,18 +19,7 @@ ricker_multi<- bf(
   alphaik ~ 1 + env,
   nl=T)
 
-
-hassell_multi<- bf(
-  totalseeds~ (exp(lambda)) / ((1 + (alphaii*conspecifics) + (alphaij*heterospecifics) + (alphaik*totalother) )^  exp(beta)),
-  lambda  ~ 1 + env,
-  alphaii ~ 1 + env,
-  alphaij ~ 1 + env,
-  alphaik ~ 1 + env,
-  beta    ~ 1 + env,
-  nl=T)
-
-
-
+# LOTKA-VOLTERRA MODEL
 lotka_volterra_multi<-bf(
   totalseeds~  exp(lambda) * (1 - (alphaii*conspecifics) - (alphaij*heterospecifics) - (alphaik*totalother))  ,
   lambda  ~ 1 + env,
@@ -37,17 +28,12 @@ lotka_volterra_multi<-bf(
   alphaij ~ 1 + env,
   nl=T)
 
-
-
-
-
-stouffer_multi <-bf(
-  totalseeds~ (exp(lambda) * (1 - ( alphaii*conspecifics) - (alphaij*heterospecifics) - (alphaik*totalother) ) ) * 
-    ( (1- exp( exp(beta)* (conspecifics + 1) )) / ( exp(beta) * (conspecifics+1) )) ,
+# HASSELL MODEL
+hassell_multi<- bf(
+  totalseeds~ (exp(lambda)) / ((1 + (alphaii*conspecifics) + (alphaij*heterospecifics) + (alphaik*totalother) )^  exp(beta)),
   lambda  ~ 1 + env,
   alphaii ~ 1 + env,
   alphaij ~ 1 + env,
-  beta    ~ 1 + env,
   alphaik ~ 1 + env,
-  nl=T
-)
+  beta    ~ 1 + env,
+  nl=T)

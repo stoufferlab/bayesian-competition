71fdfee5c475a7f22a879600e96010b615afc288
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2022-12-01 09:29:04 +1300

    streamlined code that fits models to fecundity data

---
+++ code/fit_models/fit_density_dependent_models.R
@@ -0,0 +1,53 @@
+
+# packages used for parallelization
+library(doMC)
+registerDoMC(cores = 2)
+
+# for our own functions
+require(here)
+source(here("code/fit_models/clean_data.R"))
+source(here("code/fit_models/brms_model_formulas.R"))
+source(here("code/fit_models/brms_model_priors.R"))
+source(here("code/fit_models/brms_model_inits.R"))
+
+# we will only fit models listed below
+models <- c(
+  "ricker",
+  "beverton_holt",
+  "interaction_free"
+)
+
+# iterate over models to be fit
+foreach(model_name = models) %:%
+  # fit each model for each focal species
+  foreach(focal = c("vero", "trcy")) %dopar% {
+    # use the appropriate dataset
+    if(focal=="vero"){
+      data <- vero_focal
+    }else{
+      data <- trcy_focal
+    }
+
+    message(paste("fitting",model_name,"model to focal species",focal,collapse=" "))
+    
+    # fit the model with brms
+    brmsmodel <- brm(
+      formula = model_formula(model_name),
+      prior = model_prior(model_name),
+      data =  data,
+      family = negbinomial(link = "identity"),
+      # silent=2,
+      # refresh=0,
+      init = model_init(model_name, data, chains=4),
+      iter = 4000,
+      warmup = 2000,
+      cores   = 4,
+      chains  = 4,
+      control = list(adapt_delta=0.999, max_treedepth=25)
+    )
+
+    # save the model object
+    id <- paste0("code/fit_models/model_objects/",focal,"_",model_name,".rds")
+    saveRDS(brmsmodel, file = here(id))
+  # }
+}

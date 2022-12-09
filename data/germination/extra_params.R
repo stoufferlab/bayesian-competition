09b57357996fef8cdbc1120618a940df01b49dd1
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2022-12-09 13:47:34 +1300

    adding code that provides the germination, seed survival, and maximum density parameters from one common location

---
+++ data/germination/extra_params.R
@@ -0,0 +1,17 @@
+
+# isaac's mean estimates
+seed_survival <- c(
+    vero = 0.9654804,
+    trcy = 0.9693324
+)
+
+germination <- c(
+    vero = 0.9641188,
+    trcy = 0.4743974
+)
+
+# maximum allowable number of PLANTS at equilibrium
+gN_max <- c(
+    vero = 500,
+    trcy = 500
+)
